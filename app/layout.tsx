cb62b8471b1ef83d5e33125b5519e4f6749dfb70
Umananda Siddha <umanandasiddha243@gmail.com>
2026-05-09 10:12:30 +0530

    feat: added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { Inter, JetBrains_Mono, Instrument_Serif } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import ClientFX from "@/app/components/ClientFX";
 import "./globals.css";
 
@@ -116,6 +117,7 @@ export default function RootLayout({
       className={`${inter.variable} ${jetbrainsMono.variable} ${instrumentSerif.variable}`}
     >
       <body>
+        <Analytics />
         <ClientFX />
         {children}
         <script
